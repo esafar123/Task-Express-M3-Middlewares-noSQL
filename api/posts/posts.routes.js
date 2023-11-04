3ccc858d6234bcd4d140026f9d14a0df2602a4d7
Ibrahim <ebrahimmsafar@gmail.com>
2023-11-04 14:06:15 +0300

    done

--- api/posts/posts.routes.js
+++ api/posts/posts.routes.js
@@ -1,17 +1,33 @@
-const express = require('express');
+const express = require("express");
+const Post = require("../../models/Post");
+
 const router = express.Router();
 const {
   postsGet,
   postsUpdate,
   postsDelete,
   postsCreate,
-} = require('./posts.controllers');
+  fetchPost,
+  postGet,
+} = require("./posts.controllers");
 
-router.get('/', postsGet);
-router.post('/', postsCreate);
+router.param("postId", async (req, res, next, postId) => {
+  try {
+    const foundPost = await Post.findById(postId);
+    if (!foundPost) {
+      return next({ status: 404, message: "post not found" });
+    }
+    req.post = foundPost;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.delete('/:postId', postsDelete);
-
-router.put('/:postId', postsUpdate);
+router.get("/", postsGet);
+router.post("/", postsCreate);
+router.delete("/:postId", postsDelete);
+router.put("/:postId", postsUpdate);
+router.get("/:postId", postGet);
 
 module.exports = router;
